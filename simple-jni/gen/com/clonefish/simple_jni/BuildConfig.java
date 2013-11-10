4067dd5419ac571a860b1788e0841d0941fe42f1
Denis Zabirokhin <freeppe@fastmail.fm>
2013-11-10 18:03:11 +0400

    Add Android prj for Issue #1: Вызывать методы из C++

---
+++ simple-jni/gen/com/clonefish/simple_jni/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.clonefish.simple_jni;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
